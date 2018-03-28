30ed1313557f2e38496ed93b8899a16937e69bee
xielbs <928135874@qq.com>
2018-03-28 19:24:50 +0800

    新增

---
+++ src/main/java/com/huayun/sample/common/ResultVo.java
@@ -0,0 +1,11 @@
+package com.huayun.sample.common;
+
+import java.io.Serializable;
+
+public class ResultVo implements Serializable {
+
+    private static final long serialVersionUID = -4230766285788208031L;
+
+
+
+}
